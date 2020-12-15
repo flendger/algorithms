9e72bb0e0c3ef265046b7e3861d92dbbfd779aa8
flendger <kiru.ee@yandex.ru>
2020-12-15 15:12:04 +0300

    Добавлена проверка на сбалансированность дерева Добавлен вывод процента сбалансированных деревьев в массиве

--- src/main/java/lesson6/MyTreeMap.java
+++ src/main/java/lesson6/MyTreeMap.java
@@ -21,6 +21,27 @@ public class MyTreeMap<Key extends Comparable<Key>, Value> {
         }
     }
 
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    private boolean isBalanced(Node node) {
+        if (node == null) return true;
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return false;
+        }
+
+        if (! isBalanced(node.left)) {
+            return false;
+        }
+
+        return isBalanced(node.right);
+    }
+
     public int size() {
         return size(root);
     }
